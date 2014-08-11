44e31a8c1314ad23bb4766df5d7ae45f6f9d913e
Richeve Bebedor <richeve.bebedor@gmail.com>
2014-08-11 16:31:43 +0800

    Initial commit with read-build-file.js

---
+++ read-build-file.js
@@ -0,0 +1,93 @@
+/*:
+	@module-license:
+		The MIT License (MIT)
+
+		Copyright (c) 2014 Richeve Siodina Bebedor
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
+	@module-configuration:
+		{
+			"packageName": "parse-object-command-format",
+			"fileName": "parse-object-command-format.js",
+			"moduleName": "parseObjectCommandFormat",
+			"authorName": "Richeve S. Bebedor",
+			"authorEMail": "richeve.bebedor@gmail.com",
+			"contributorList": [
+				{
+					"contributorName": "Geoff Diaz",
+					"contributorEMail": "",
+				}
+			],
+			"repository": "git@github.com:volkovasystems/parse-object-command-format.git"
+		}
+	@end-module-configuration
+
+	@module-documentation:
+
+	@end-module-documentation
+
+	@include:
+		{
+			"fs@nodejs": "fs",
+			"path@nodejs": "path"
+		}
+	@end-include
+*/
+var readBuildFile = function readBuildFile( domain, buildFilePath ){
+	/*:
+		@meta-configuration:
+			{
+				"domain:required": "string",
+				"buildFilePath:optional": "string"
+			}
+		@end-meta-configuration
+	*/
+
+	var domainPattern = new RegExp( domain + "$" );
+	var currentWorkingDirectory = process.cwd( );
+
+	if( typeof buildFilePath == "string" ){
+		var domainBasedOnBuildFilePath = buildFilePath.split( path.sep ).pop( );
+		if( domainPattern.test( domainBasedOnBuildFilePath ) ){
+			process.chdir( buildFilePath );
+		}
+	}
+
+	for( var levelCount = 0; levelCount < 5; levelCount++ ){
+		process.chdir( process.cwd( ).split( path.sep ).slice( 0, -1 ).join( path.sep ) );
+		if( !domainPattern.test( process.cwd( ) ) ){
+			break;
+		}
+	}
+
+	if( levelCount > 5 ){
+		var error = new Error( "reading build file called without a build file" );
+		console.error( error );
+		throw error;
+	}
+
+
+};
+
+var fs = require( "fs" );
+var path = require( "path" );
+
+module.exports = readBuildFile;
